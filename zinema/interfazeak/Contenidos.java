5d4fa54057342168ef199ced990b7098ea2d8ea6
Aritz-Garcia <aritz.garciaba@elorrieta-errekamari.com>
2022-05-05 12:05:43 +0300

    java: contenidos + egun aukeraketa

--- zinema/interfazeak/Contenidos.java
+++ zinema/interfazeak/Contenidos.java
@@ -1,7 +1,6 @@
 package interfazeak;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
@@ -12,12 +11,20 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.Font;
 import javax.swing.Box;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+
 import java.awt.Component;
 import java.awt.Dimension;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.ComponentOrientation;
 
 public class Contenidos {
 
@@ -51,7 +58,7 @@ public class Contenidos {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 350);
+		frame.setBounds(100, 100, 576, 332);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
@@ -64,9 +71,9 @@ public class Contenidos {
 		contentPane.add(panel, BorderLayout.NORTH);
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{20, 46, 115, 0, 20, 0};
-		gbl_panel.rowHeights = new int[]{0, 14, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 14, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
 		JLabel lblRegistro = new JLabel("CONTENIDOS");
@@ -74,95 +81,175 @@ public class Contenidos {
 		lblRegistro.setAlignmentX(0.5f);
 		GridBagConstraints gbc_lblRegistro = new GridBagConstraints();
 		gbc_lblRegistro.insets = new Insets(0, 0, 5, 5);
-		gbc_lblRegistro.gridx = 2;
+		gbc_lblRegistro.gridx = 1;
 		gbc_lblRegistro.gridy = 1;
 		panel.add(lblRegistro, gbc_lblRegistro);
 		
-		JLabel lblPelicDisp = new JLabel("Peliculas Disponibles:");
-		lblPelicDisp.setFont(new Font("Tahoma", Font.BOLD, 13));
-		GridBagConstraints gbc_lblPelicDisp = new GridBagConstraints();
-		gbc_lblPelicDisp.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPelicDisp.gridx = 2;
-		gbc_lblPelicDisp.gridy = 3;
-		panel.add(lblPelicDisp, gbc_lblPelicDisp);
+		ArrayList<String> ArrayPelikulak = new ArrayList<>();
+		ArrayPelikulak.add("Peli1");
+		ArrayPelikulak.add("Peli2");
+		ArrayPelikulak.add("Peli3");
+		ArrayPelikulak.add("Peli4");
+		ArrayPelikulak.add("Peli5");
+		
+		JButton btnBueltatu = new JButton("Bueltatu Egun Aukeraketara");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_1.gridx = 2;
+		gbc_btnNewButton_1.gridy = 1;
+		panel.add(btnBueltatu, gbc_btnNewButton_1);
+		
+		btnBueltatu.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){
+            	EgunaAukeratu.main(null);
+            }
+		});
 		
 		Box horizontalBox = Box.createHorizontalBox();
 		GridBagConstraints gbc_horizontalBox = new GridBagConstraints();
 		gbc_horizontalBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_horizontalBox.insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox.gridx = 2;
-		gbc_horizontalBox.gridy = 4;
+		gbc_horizontalBox.gridx = 1;
+		gbc_horizontalBox.gridy = 3;
 		panel.add(horizontalBox, gbc_horizontalBox);
 		
-		JLabel lblPeliculak = new JLabel("Aukeratu pelikula:");
-		horizontalBox.add(lblPeliculak);
+
+		JLabel lblPelicDisp = new JLabel("Aukeratu pelikula:");
+		lblPelicDisp.setFont(new Font("Tahoma", Font.BOLD, 13));
+		horizontalBox.add(lblPelicDisp);
 		
 		Component rigidArea = Box.createRigidArea(new Dimension(10, 20));
 		horizontalBox.add(rigidArea);
-		
-		JComboBox comboBoxPelikulak = new JComboBox();
+		JComboBox comboBoxPelikulak = new JComboBox(ArrayPelikulak.toArray());
 		horizontalBox.add(comboBoxPelikulak);
 		
-		Box horizontalBox_1 = Box.createHorizontalBox();
-		GridBagConstraints gbc_horizontalBox_1 = new GridBagConstraints();
-		gbc_horizontalBox_1.insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_1.gridx = 2;
-		gbc_horizontalBox_1.gridy = 5;
-		panel.add(horizontalBox_1, gbc_horizontalBox_1);
+		JButton btnEdukiaEditatu = new JButton("Edukia Editatu");
+		GridBagConstraints gbc_btnEdukiaEditatu = new GridBagConstraints();
+		gbc_btnEdukiaEditatu.anchor = GridBagConstraints.WEST;
+		gbc_btnEdukiaEditatu.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEdukiaEditatu.gridx = 1;
+		gbc_btnEdukiaEditatu.gridy = 5;
+		panel.add(btnEdukiaEditatu, gbc_btnEdukiaEditatu);
 		
-		JButton btnEditar = new JButton("Editar");
-		horizontalBox_1.add(btnEditar);
 		
-		Component rigidArea_1 = Box.createRigidArea(new Dimension(15, 20));
-		horizontalBox_1.add(rigidArea_1);
+		Box verticalBox = Box.createVerticalBox();
+		verticalBox.setVisible(false);
+		GridBagConstraints gbc_verticalBox = new GridBagConstraints();
+		gbc_verticalBox.anchor = GridBagConstraints.WEST;
+		gbc_verticalBox.gridheight = 5;
+		gbc_verticalBox.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalBox.gridx = 1;
+		gbc_verticalBox.gridy = 6;
+		panel.add(verticalBox, gbc_verticalBox);
 		
-		JButton btnBorrar = new JButton("Borrar");
-		horizontalBox_1.add(btnBorrar);
-		
-		Component rigidArea_1_1 = Box.createRigidArea(new Dimension(15, 20));
-		horizontalBox_1.add(rigidArea_1_1);
-		
-		JButton btnNuevo = new JButton("Nuevo");
-		horizontalBox_1.add(btnNuevo);
-		
-		Box horizontalBox_2 = Box.createHorizontalBox();
-		GridBagConstraints gbc_horizontalBox_2 = new GridBagConstraints();
-		gbc_horizontalBox_2.insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_2.gridx = 2;
-		gbc_horizontalBox_2.gridy = 6;
-		panel.add(horizontalBox_2, gbc_horizontalBox_2);
-		
-		JButton btnCancelar = new JButton("Cancelar");
-		horizontalBox_2.add(btnCancelar);
+		JButton btnEditar = new JButton("Editatu");
+		verticalBox.add(btnEditar);
 		
 		Component rigidArea_2 = Box.createRigidArea(new Dimension(20, 20));
-		horizontalBox_2.add(rigidArea_2);
+		verticalBox.add(rigidArea_2);
 		
-		JButton btnConfirmar = new JButton("Confirmar");
+		JButton btnBorrar = new JButton("Ezabatu");
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (comboBoxPelikulak.getSelectedIndex() > -1) {
+			        ArrayPelikulak.remove(comboBoxPelikulak.getSelectedIndex());
+			        comboBoxPelikulak.getSelectedIndex();
+			        comboBoxPelikulak.setModel(new DefaultComboBoxModel(ArrayPelikulak.toArray()));
+				}
+			}
+		});
+		verticalBox.add(btnBorrar);
+		
+		Component rigidArea_3 = Box.createRigidArea(new Dimension(20, 20));
+		verticalBox.add(rigidArea_3);
+		
+		JButton btnNuevo = new JButton("Berria");
+		verticalBox.add(btnNuevo);
+		
+		JLabel lblLogo1 = new JLabel("");
+		lblLogo1.setBackground(Color.WHITE);
+		lblLogo1.setIcon(new ImageIcon(Confirmacion.class.getResource("../imagenes/logoa/logo_pequeno.png")));
+		GridBagConstraints gbc_lblLogo1 = new GridBagConstraints();
+		gbc_lblLogo1.gridheight = 8;
+		gbc_lblLogo1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLogo1.gridx = 2;
+		gbc_lblLogo1.gridy = 5;
+		panel.add(lblLogo1, gbc_lblLogo1);
+		
+		Box horizontalBox_2 = Box.createHorizontalBox();
+		horizontalBox_2.setVisible(false);
+		GridBagConstraints gbc_horizontalBox_2 = new GridBagConstraints();
+		gbc_horizontalBox_2.anchor = GridBagConstraints.WEST;
+		gbc_horizontalBox_2.insets = new Insets(0, 0, 5, 5);
+		gbc_horizontalBox_2.gridx = 1;
+		gbc_horizontalBox_2.gridy = 11;
+		panel.add(horizontalBox_2, gbc_horizontalBox_2);
+		
+		JButton btnCancelar = new JButton("Kantzelatu");
+		horizontalBox_2.add(btnCancelar);
+		
+		Component rigidArea_4 = Box.createRigidArea(new Dimension(20, 20));
+		horizontalBox_2.add(rigidArea_4);
+		
+		JButton btnConfirmar = new JButton("Baieztatu");
 		horizontalBox_2.add(btnConfirmar);
 		
-		Box horizontalBox_3 = Box.createHorizontalBox();
-		GridBagConstraints gbc_horizontalBox_3 = new GridBagConstraints();
-		gbc_horizontalBox_3.insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_3.gridx = 2;
-		gbc_horizontalBox_3.gridy = 8;
-		panel.add(horizontalBox_3, gbc_horizontalBox_3);
+		Box verticalBox_2 = Box.createVerticalBox();
+		GridBagConstraints gbc_verticalBox_2 = new GridBagConstraints();
+		gbc_verticalBox_2.anchor = GridBagConstraints.WEST;
+		gbc_verticalBox_2.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalBox_2.gridx = 1;
+		gbc_verticalBox_2.gridy = 12;
+		panel.add(verticalBox_2, gbc_verticalBox_2);
 		
-		JButton btnAnadirPelic = new JButton("A\u00F1adir Pelicula");
-		horizontalBox_3.add(btnAnadirPelic);
+		JButton btnAnadirPelic = new JButton("Pelikula gehitu");
+		btnAnadirPelic.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		verticalBox_2.add(btnAnadirPelic);
 		
-		Component rigidArea_3 = Box.createRigidArea(new Dimension(10, 20));
-		horizontalBox_3.add(rigidArea_3);
+		Component rigidArea_5 = Box.createRigidArea(new Dimension(10, 10));
+		verticalBox_2.add(rigidArea_5);
 		
-		JButton btnResDia = new JButton("Resumen del D\u00EDa");
+		JButton btnResDia = new JButton("Eguneko Laburpena");
 		btnResDia.setEnabled(false);
-		horizontalBox_3.add(btnResDia);
+		verticalBox_2.add(btnResDia);
+		
 		
 		btnEditar.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){
             	frame.setVisible(false);
             	EdukiaEditatu.main(null);
             }
+		});
+		
+		btnNuevo.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){
+            	EdukiBerria.main(null);
+            }
+		});
+		
+		btnEdukiaEditatu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (verticalBox_2.isVisible()) {
+					verticalBox_2.setVisible(false);
+					
+					verticalBox.setVisible(true);
+					horizontalBox_2.setVisible(true);
+				}
+			}
+		});
+		
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (verticalBox.isVisible() || horizontalBox_2.isVisible()) {
+					verticalBox.setVisible(false);
+					horizontalBox_2.setVisible(false);
+					
+					verticalBox_2.setVisible(true);
+				}
+			}
 		});
 		
 	}
--- zinema/interfazeak/EgunaAukeratu.java
+++ zinema/interfazeak/EgunaAukeratu.java
@@ -7,9 +7,10 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.Box;
-import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -20,6 +21,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 public class EgunaAukeratu {
 
@@ -53,7 +55,7 @@ public class EgunaAukeratu {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 360);
+		frame.setBounds(100, 100, 528, 365);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel contentPane = new JPanel();
@@ -62,22 +64,34 @@ public class EgunaAukeratu {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.NORTH);
+		contentPane.add(panel, BorderLayout.CENTER);
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{20, 46, 115, 0, 20, 0};
-		gbl_panel.rowHeights = new int[]{0, 14, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 14, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
+		
+		/*------------------------------------------------------------------------------------------------------*/
 		
 		JLabel lblRegistro = new JLabel("Egun Aukeraketa");
 		lblRegistro.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblRegistro.setAlignmentX(0.5f);
 		GridBagConstraints gbc_lblRegistro = new GridBagConstraints();
 		gbc_lblRegistro.insets = new Insets(0, 0, 5, 5);
-		gbc_lblRegistro.gridx = 2;
+		gbc_lblRegistro.gridx = 1;
 		gbc_lblRegistro.gridy = 1;
 		panel.add(lblRegistro, gbc_lblRegistro);
+		
+		ArrayList<String> ArrayEnpresaKargua = new ArrayList<>();
+		ArrayEnpresaKargua.add("");
+		ArrayEnpresaKargua.add("Astelehena");
+		ArrayEnpresaKargua.add("Asteartea");
+		ArrayEnpresaKargua.add("Asteazkena");
+		ArrayEnpresaKargua.add("Osteguna");
+		ArrayEnpresaKargua.add("Ostirala");
+		ArrayEnpresaKargua.add("Larunbata");
+		ArrayEnpresaKargua.add("Igandea");
 		
 		/*------------------------------------------------------------------------------------------------------*/
 		
@@ -85,7 +99,7 @@ public class EgunaAukeratu {
 		GridBagConstraints gbc_horizontalBox_1 = new GridBagConstraints();
 		gbc_horizontalBox_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_horizontalBox_1.insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_1.gridx = 2;
+		gbc_horizontalBox_1.gridx = 1;
 		gbc_horizontalBox_1.gridy = 3;
 		panel.add(horizontalBox_1, gbc_horizontalBox_1);
 		
@@ -94,21 +108,24 @@ public class EgunaAukeratu {
 		
 		Component rigidArea_4 = Box.createRigidArea(new Dimension(10, 20));
 		horizontalBox_1.add(rigidArea_4);
+		JComboBox comboBoxEnpresaKargua = new JComboBox(ArrayEnpresaKargua.toArray());
 		
-		JComboBox comboBoxEnpresaKargua = new JComboBox();
-		comboBoxEnpresaKargua.setModel(new DefaultComboBoxModel(new String[] {"", "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata", "Igandea"}));
-		comboBoxEnpresaKargua.setName("");
+		
 		horizontalBox_1.add(comboBoxEnpresaKargua);
 		
-		/*------------------------------------------------------------------------------------------------------*/
+		Box horizontalBox_4 = Box.createHorizontalBox();
+		horizontalBox_4.setVisible(false);
+		
+		Box horizontalBox_3 = Box.createHorizontalBox();
+		horizontalBox_3.setVisible(false);
 		
 		Box horizontalBox_2 = Box.createHorizontalBox();
 		horizontalBox_2.setVisible(false);
 		GridBagConstraints gbc_horizontalBox_2  = new GridBagConstraints();
 		gbc_horizontalBox_2 .fill = GridBagConstraints.HORIZONTAL;
 		gbc_horizontalBox_2 .insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_2 .gridx = 2;
-		gbc_horizontalBox_2 .gridy = 4;
+		gbc_horizontalBox_2 .gridx = 1;
+		gbc_horizontalBox_2 .gridy = 5;
 		panel.add(horizontalBox_2 , gbc_horizontalBox_2);
 		
 		JLabel lblEdukiak = new JLabel("Eguneko edukiak: ");
@@ -117,18 +134,13 @@ public class EgunaAukeratu {
 		Component rigidArea = Box.createRigidArea(new Dimension(10, 20));
 		horizontalBox_2 .add(rigidArea);
 		
-		JLabel lblNewLabel = new JLabel("");
-		horizontalBox_2.add(lblNewLabel);
-		
-		/*------------------------------------------------------------------------------------------------------*/
-		
-		Box horizontalBox_3 = Box.createHorizontalBox();
-		horizontalBox_3.setVisible(false);
+		JLabel lblEdukiakErantz = new JLabel("");
+		horizontalBox_2.add(lblEdukiakErantz);
 		GridBagConstraints gbc_horizontalBox_3  = new GridBagConstraints();
 		gbc_horizontalBox_3 .fill = GridBagConstraints.HORIZONTAL;
 		gbc_horizontalBox_3 .insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_3 .gridx = 2;
-		gbc_horizontalBox_3 .gridy = 5;
+		gbc_horizontalBox_3 .gridx = 1;
+		gbc_horizontalBox_3 .gridy = 6;
 		panel.add(horizontalBox_3 , gbc_horizontalBox_3);
 		
 		JLabel lblEduki_kop = new JLabel("Eduki kopurua: ");
@@ -137,18 +149,13 @@ public class EgunaAukeratu {
 		Component rigidArea_2 = Box.createRigidArea(new Dimension(10, 20));
 		horizontalBox_3 .add(rigidArea_2);
 		
-		JLabel lblNewLabel_1 = new JLabel("");
-		horizontalBox_3.add(lblNewLabel_1);
-		
-		/*------------------------------------------------------------------------------------------------------*/
-		
-		Box horizontalBox_4 = Box.createHorizontalBox();
-		horizontalBox_4.setVisible(false);
+		JLabel lblEdKopErantz = new JLabel("");
+		horizontalBox_3.add(lblEdKopErantz);
 		GridBagConstraints gbc_horizontalBox_4  = new GridBagConstraints();
 		gbc_horizontalBox_4 .fill = GridBagConstraints.HORIZONTAL;
 		gbc_horizontalBox_4 .insets = new Insets(0, 0, 5, 5);
-		gbc_horizontalBox_4 .gridx = 2;
-		gbc_horizontalBox_4 .gridy = 6;
+		gbc_horizontalBox_4 .gridx = 1;
+		gbc_horizontalBox_4 .gridy = 7;
 		panel.add(horizontalBox_4 , gbc_horizontalBox_4);
 		
 		JLabel lblBatazBesteko_iraupena = new JLabel("Bataz-besteko iraunpena: ");
@@ -157,16 +164,41 @@ public class EgunaAukeratu {
 		Component rigidArea_3 = Box.createRigidArea(new Dimension(10, 20));
 		horizontalBox_4 .add(rigidArea_3);
 		
-		JLabel lblNewLabel_2 = new JLabel("");
-		horizontalBox_4.add(lblNewLabel_2);
+		JLabel lblBatBestIraupErantz = new JLabel("");
+		horizontalBox_4.add(lblBatBestIraupErantz);
+		/*if(comboBoxEnpresaKargua.getSelectedIndex() > 0)
+			btnKartelera.setEnabled(true);
+			
+		else
+			btnKartelera.setEnabled(false);*/
 		
 		/*------------------------------------------------------------------------------------------------------*/
 		
+		JLabel lblLogo = new JLabel("");
+		lblLogo.setBackground(Color.WHITE);
+		lblLogo.setIcon(new ImageIcon(Confirmacion.class.getResource("../imagenes/logoa/logo_pequeno.png")));
+		GridBagConstraints gbc_lblLogo = new GridBagConstraints();
+		gbc_lblLogo.gridheight = 6;
+		gbc_lblLogo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLogo.gridx = 2;
+		gbc_lblLogo.gridy = 2;
+		panel.add(lblLogo, gbc_lblLogo);
+		
+		Box horizontalBox_filler = Box.createHorizontalBox();
+		GridBagConstraints gbc_horizontalBox_filler = new GridBagConstraints();
+		gbc_horizontalBox_filler.insets = new Insets(0, 0, 5, 5);
+		gbc_horizontalBox_filler.gridx = 1;
+		gbc_horizontalBox_filler.gridy = 8;
+		panel.add(horizontalBox_filler, gbc_horizontalBox_filler);
+		
+		Component rigidArea_filler = Box.createRigidArea(new Dimension(10, 20));
+		horizontalBox_filler.add(rigidArea_filler);
+		
 		Box horizontalBox_5 = Box.createHorizontalBox();
 		GridBagConstraints gbc_horizontalBox_5 = new GridBagConstraints();
-		gbc_horizontalBox_5.insets = new Insets(0, 0, 0, 5);
-		gbc_horizontalBox_5.gridx = 2;
-		gbc_horizontalBox_5.gridy = 8;
+		gbc_horizontalBox_5.insets = new Insets(0, 0, 5, 5);
+		gbc_horizontalBox_5.gridx = 1;
+		gbc_horizontalBox_5.gridy = 9;
 		panel.add(horizontalBox_5, gbc_horizontalBox_5);
 		
 		JButton btnHasiera = new JButton("Hasiera");
@@ -179,25 +211,27 @@ public class EgunaAukeratu {
 		btnKartelera.setEnabled(false);
 		horizontalBox_5.add(btnKartelera);
 		
-		/*------------------------------------------------------------------------------------------------------*/
+		btnHasiera.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){
+            	frame.setVisible(false);
+            	Ongietorria.main(null);
+            }
+		});
 		
-		Box horizontalBox_filler = Box.createHorizontalBox();
-		GridBagConstraints gbc_horizontalBox_filler = new GridBagConstraints();
-		gbc_horizontalBox_filler.insets = new Insets(0, 0, 0, 5);
-		gbc_horizontalBox_filler.gridx = 2;
-		gbc_horizontalBox_filler.gridy = 9;
-		panel.add(horizontalBox_filler, gbc_horizontalBox_filler);
-		
-		Component rigidArea_filler = Box.createRigidArea(new Dimension(10, 20));
-		horizontalBox_filler.add(rigidArea_filler);
+		btnKartelera.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){
+            	frame.setVisible(false);
+            	Contenidos.main(null);
+            }
+		});
 		
 		/*------------------------------------------------------------------------------------------------------*/
 		
 		Box horizontalBox_6 = Box.createHorizontalBox();
 		GridBagConstraints gbc_horizontalBox_6 = new GridBagConstraints();
-		gbc_horizontalBox_6.insets = new Insets(0, 0, 0, 5);
+		gbc_horizontalBox_6.insets = new Insets(0, 0, 5, 5);
 		gbc_horizontalBox_6.gridx = 2;
-		gbc_horizontalBox_6.gridy = 10;
+		gbc_horizontalBox_6.gridy = 9;
 		panel.add(horizontalBox_6, gbc_horizontalBox_6);
 		
 		JButton btnAsteko_laburpena = new JButton("Asteko laburpena");
@@ -214,32 +248,47 @@ public class EgunaAukeratu {
 		btnBalioztatu.setEnabled(false);
 		horizontalBox_6.add(btnBalioztatu);
 		
-		btnHasiera.addActionListener(new ActionListener(){  
-            public void actionPerformed(ActionEvent e){
-            	frame.setVisible(false);
-            	Ongietorria.main(null);
-            }
-		});
-		
-		btnKartelera.addActionListener(new ActionListener(){  
-            public void actionPerformed(ActionEvent e){
-            	frame.setVisible(false);
-            	Contenidos.main(null);
-            }
-		});
-		
-		btnAsteko_laburpena.addActionListener(new ActionListener(){  
-            public void actionPerformed(ActionEvent e){
-            	frame.setVisible(false);
-            	Resumen_Semana.main(null);
-            }
-		});
-		
 		btnBalioztatu.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){
             	frame.setVisible(false);
             	Confirmacion.main(null);
             }
+		});
+		
+		comboBoxEnpresaKargua.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(comboBoxEnpresaKargua.getSelectedIndex() > 0)
+					btnKartelera.setEnabled(true);
+				else
+					btnKartelera.setEnabled(false);
+			}
+		});
+		
+		comboBoxEnpresaKargua.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(comboBoxEnpresaKargua.getSelectedIndex() > 0)
+					horizontalBox_2.setVisible(true);
+				else
+					horizontalBox_2.setVisible(false);
+			}
+		});
+		
+		comboBoxEnpresaKargua.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(comboBoxEnpresaKargua.getSelectedIndex() > 0)
+					horizontalBox_3.setVisible(true);
+				else
+					horizontalBox_3.setVisible(false);
+			}
+		});
+		
+		comboBoxEnpresaKargua.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(comboBoxEnpresaKargua.getSelectedIndex() > 0)
+					horizontalBox_4.setVisible(true);
+				else
+					horizontalBox_4.setVisible(false);
+			}
 		});
 	}
 
